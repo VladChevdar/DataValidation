6092ba1de569788447fe05d1e427ed8e548555bb
Vlad Chevdar <143352246+VladChevdar@users.noreply.github.com>
2025-04-21 18:16:03 -0700

    Update emp_validate.py

--- emp_validate.py
+++ emp_validate.py
@@ -1,16 +1,19 @@
 import pandas as pd
 
+# Load data
 df = pd.read_csv('employees.csv')
 known_eids = set(df['eid'])
 city_counts = df['city'].value_counts()
 df = df.dropna(subset=['salary'])
 
+# Calculate
 num_null_names = df['name'].isnull().sum()
 num_hired_before_2015 = (df['hire_date'] < '2015-01-01').sum()
 num_hired_before_birth = (df['hire_date'] < df['birth_date']).sum()
 num_unknown_managers = ((df['reports_to'].notnull()) & (~df['reports_to'].isin(known_eids))).sum()
 num_one_emp_cities = len(city_counts[city_counts == 1])
 
+# Output
 print(f"Employees with missing name: {num_null_names}")
 print(f"Employees hired before 2015: {num_hired_before_2015}")
 print(f"Employees hired before their birth date: {num_hired_before_birth}")
